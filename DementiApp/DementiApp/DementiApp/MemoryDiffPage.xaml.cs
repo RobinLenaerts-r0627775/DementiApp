72c35c2b450e020252d17d996f30b128d79afea6
RobinLenaerts-r0627775 <robin.lenaerts1@student.ucll.be>
2019-02-25 14:31:08 +0100

    changes with last commit

---
+++ DementiApp/DementiApp/DementiApp/MemoryDiffPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DementiApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MemoryDiffPage : ContentPage
+	{
+		public MemoryDiffPage ()
+		{
+			InitializeComponent ();
+		}
+        
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MemoryEasyPage());
+        }
+
+        async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MemoryMediumPage());
+        }
+
+        async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MemoryPage());
+        }
+    }
+}
---
+++ DementiWeb/build/resources/main/static/js/JSONscript.js
@@ -0,0 +1,76 @@
+var editButton = document.getElementById("edit");
+var addButton = document.getElementById("save");
+var form = document.getElementById("form");
+
+var xhrObj = new XMLHttpRequest();
+
+function savePatient() {
+    var formDataSerAr = $("form").serializeArray();
+    var result = {};
+    $.each(formDataSerAr, function () {
+        if (result[this.name]) {
+            if (!result[this.name].push) {
+                result[this.name] = [o[this.name]];
+            }
+            result[this.name].push(this.value || '');
+        } else {
+            result[this.name] = this.value || '';
+        }
+    });
+    console.log(result);
+    $.ajax({
+        url:"http://localhost:8080/patients",
+        type:"POST",
+        data: JSON.stringify(result),
+        contentType: 'application/json',
+        success: function (data) {
+            if (data.redirect){
+                console.log("1");
+                window.location.href = data.redirect;
+            } else {
+                console.log("2");
+                window.location.replace("/patients");
+            }
+        },
+        error: function(data){
+            console.log(data);
+            alert("An error occured");
+        }
+    });
+    //$("<a href='/patients'></a>").click();
+}
+
+function editPatient() {
+    var formDataSerAr = $("form").serializeArray();
+    var result = {};
+    $.each(formDataSerAr, function () {
+        if (result[this.name]) {
+            if (!result[this.name].push) {
+                result[this.name] = [o[this.name]];
+            }
+            result[this.name].push(this.value || '');
+        } else {
+            result[this.name] = this.value || '';
+        }
+    });
+    $.ajax({
+        url:"http://localhost:8080/patients",
+        type:"PUT",
+        data: JSON.stringify(result),
+        contentType: 'application/json',
+        success: function (result) {
+        }
+    });
+}
+
+
+
+/*function getData(){
+    var patientId = document.getElementById(patientId);
+    var firstName = document.getElementById(firstName);
+    var lastName = document.getElementById(lastName);
+    var birthDate = document.getElementById(birthDate);
+    var dementiaLevel = document.getElementById(dementiaLevel);
+
+    var result = "";
+}*/
