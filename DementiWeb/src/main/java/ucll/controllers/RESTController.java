6731febd8097516d4cd33f8d5b400928a5d176f2
RobbeCuyvers-r0673295 <robbe.cuyvers@student.ucll.be>
2019-02-26 14:08:33 +0100

    Path fix

--- DementiWeb/src/main/java/ucll/controllers/RESTController.java
+++ DementiWeb/src/main/java/ucll/controllers/RESTController.java
@@ -34,7 +34,7 @@ import java.util.stream.StreamSupport;
 @RequestMapping("/api")
 public class RESTController {
     //private static String fileDir = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\images\\";  //local
-    private static String fileDir = System.getProperty("user.dir") + "\\build\\resources\\main\\static\\images\\";    //server
+    private static String fileDir = System.getProperty("user.dir") + "build/resources/main/static/images/";    //server
 
 
     @Autowired //Inject?
